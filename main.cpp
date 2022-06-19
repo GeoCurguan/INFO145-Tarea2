79db334b1ebb8b56607f6257443c77c29d31849e
faxio <the_fabio@live.cl>
2022-06-18 22:27:29 -0400

    Codigo 3 listo "aparentemente"

--- main.cpp
+++ main.cpp
@@ -5,6 +5,23 @@
 #include <set>
 using namespace std;
 
+//funcion que imprime sets
+void imprimirSets(set<int> x){
+    set<int>::iterator ite;
+    for (ite = x.begin(); ite != x.end(); ite++) {
+        cout << *ite << " ";
+    }
+    cout << endl;
+}
+void imprimirVector(vector<set<int>> F){
+    set<int> element;
+     for (size_t i = 0; i < F.size(); i++){
+        element.insert(F.at(i).begin(),F.at(i).end());
+        imprimirSets(element);
+        element.clear();
+    } 
+}
+
 //Funcion que retorna el universo de elementos
 set<int> getUniverse(vector<set<int>> F){
     set<int> x(F[0]);
@@ -50,9 +67,37 @@ void exhaustiveSearch(set<int> x, vector<set<int>> F){
         }
     }
 }
+void greedAlgoritms(set<int> x, vector<set<int>> F){
+    set<int> U = x;
+    vector<set<int>> C;
+    set<int> S;
+    int mayor = 0;
+    while( U.size() > 0){
+        for(size_t j = 0; j < F.size(); j++){
+            if (F.at(j).size() > mayor){
+                S.clear();
+                S.insert(F[j].begin(),F[j].end());
+                mayor = F.at(j).size();
+                    //F.erase()
+                auto elem = F.begin() + j;
+                if (elem != F.end()){
+                    F.erase(elem);
+                }
+            } 
+        }
+        mayor= 0;
+        set<int>::iterator ite;
+        for (ite = S.begin(); ite != S.end(); ite++) {
+            U.erase(*ite);
+        }
+        C.push_back(S);
+    }
+    imprimirVector(C);
+}
 
 int main(){
     // Set de ejemplos
+    /*
     set<int> A = {1,2,3};
     set<int> B = {2,3,4,5};
     set<int> C = {4,5,6};
@@ -68,6 +113,28 @@ int main(){
     set<int> x;
     x = getUniverse(F);
     exhaustiveSearch(x,F);
+*/
+        // Set de ejemplos
+    set<int> S0 = {1,2,3,4,5,6};
+    set<int> S1= {5,6,8,9};
+    set<int> S2= {1,4,7,10};
+    set<int> S3 = {2,5,7,8,11};
+    set<int> S4 = {3,6,9,12};
+    set<int> S5 = {10,11};
+    set<int>::iterator it;
+    vector<set<int>> F;
+    F.push_back(S0);
+    F.push_back(S1);
+    F.push_back(S2);
+    F.push_back(S3);
+    F.push_back(S4);
+    F.push_back(S5);
+
+    set<int> x;
+    x = getUniverse(F);
+    exhaustiveSearch(x,F);
+    //imprimirVector(F);
+    greedAlgoritms(x,F);
     /*it = s1.find(4);
     bool isIn = s1.find(3) != s1.end();
     cout << isIn << endl;
