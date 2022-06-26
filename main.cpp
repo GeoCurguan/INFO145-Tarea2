5035a25c8dc2b1984fd190cda843178832762ef5
Geovanni Curguan <curguangeo@gmail.com>
2022-06-25 21:24:54 -0400

    Sol 1 y 2 listo

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <chrono>
 #include <thread>
+#include <fstream>
 using namespace std;
 
 
@@ -60,30 +61,35 @@ vector<int> comb(int N, int K,set<int> x, vector<set<int>> F)
 
 void exhaustiveSearch(set<int> x, vector<set<int>> F){
     cout << "Comenzando solucion 1......" << endl;
-    for(size_t j = 1; j < F.size(); j++){
+    int mscSize = F.size();
+    vector<int> solMsc;
+    string solMscPrint = "";
+    for(size_t j = F.size()-1; j != 0; j--){
         vector<int> msc = comb(F.size(),j,x,F);
-        if (!msc.empty()){
-            cout << "MSC encontrado de tamaño: " << msc.size() << endl;
-            //En caso de que se quiera imprimir
-            cout << "Los conjunto que lo forman son: " << endl;
+        if(!msc.empty() && msc.size() <= mscSize){
+            solMsc = msc;
+            solMscPrint = "";
             for(size_t i = 0; i != msc.size(); i++){
-                cout << "S" << msc[i]+1 << "  ";
-                imprimirSets(F[i]);
+                solMscPrint = solMscPrint + "S" + to_string(msc[i]+1) + " ";
             }
-            cout << endl;
-            break;
+            mscSize = msc.size();
         }
+        msc.clear();
+    }
+    cout << "MSC encontrado de tamaño: " << mscSize << endl;
+    //Print menor a 60 para no generar un flood en la consola
+    if(mscSize < 60){
+        cout << solMscPrint << endl;
     }
     cout << "Fin Solucion 1" << endl << "-------------------------------------------------" << endl;
 }
 //Solucion 2
-
 void optimizedSearch(set<int> x, vector<set<int>> F){
     cout << "Comenzando solucion 2......" << endl;
-    //Buscando los elementos que se encuentren en un solo conjunto
     if(F.size() > 1){
         set<int> inter;
         set<int> setComp;
+        //Buscando los elementos que se encuentren en un solo conjunto
         set_intersection(F[0].begin(),F[0].end(),F[1].begin(),F[1].end(), inserter(inter, inter.begin()));
         set_symmetric_difference(F[0].begin(),F[0].end(),F[1].begin(),F[1].end(), inserter(setComp, setComp.begin()));
         set<int> aux;
@@ -95,8 +101,6 @@ void optimizedSearch(set<int> x, vector<set<int>> F){
             set_intersection(aux.begin(),aux.end(),F[i].begin(),F[i].end(), inserter(inter, inter.begin()));
             set_symmetric_difference(aux.begin(),aux.end(),F[i].begin(),F[i].end(), inserter(setComp, setComp.begin()));
         }
-        //Por si se quiere imprimir todos los elementos que estan en mas de un conjunto
-        //imprimirSets(inter);
         setComp.clear();
         set_difference(x.begin(),x.end(),inter.begin(),inter.end(), inserter(setComp, setComp.begin()));
         int mscSize = 0;
@@ -106,24 +110,28 @@ void optimizedSearch(set<int> x, vector<set<int>> F){
             size_t c = 0;
             //aca deberia buscar todos los sets que tengan el elemento que solo está en un solo conjunto
             while(c != F.size() && F[c].find(*k) == F[c].end()) c++;
-            solMscPrint = solMscPrint + "S" + to_string(c+1) + " ";
+            //imprimirSets(F[c]);
+            //cout << c << endl;
+            //cout << solMscPrint << endl;
             //imprimirSets(F[c]);
             if(c != F.size()){
+                solMscPrint = solMscPrint + "S" + to_string(c+1) + " ";
+                //cout << c+1 << "/" << setSol << endl;
                 set<int>::iterator iter;
                 set<int>::iterator del;
-            for(iter=F[c].begin(); iter!=F[c].end(); iter++){
-                x.erase(x.find(*iter));
-                for(size_t j=0;j < F.size(); j++){
-                    if(j != c)((del = find(F[j].begin(), F[j].end(), *iter)) == F[j].end()) ? del : F[j].erase(del);
+                for(iter=F[c].begin(); iter!=F[c].end(); iter++){
+                    x.erase(x.find(*iter));
+                    for(size_t j=0;j < F.size(); j++){
+                        if(j != c)((del = find(F[j].begin(), F[j].end(), *iter)) == F[j].end()) ? del : F[j].erase(del);
+                    }
                 }
+                F[c].clear();
+                mscSize++;
             }
-            F.erase(F.begin() + c);
-            }
-            mscSize++;
         }
-        //Si se cumple, se aplica la primera optimizacion
         for(size_t j = 1; j < F.size(); j++){
             vector<int> msc = comb(F.size(),j,x,F);
+            //Si entra al if, es porque encontro un MSC
             if (!msc.empty()){
                 cout << "MSC encontrado de tamaño: " << msc.size()+mscSize << endl;
                 //En caso de que se quiera imprimir
@@ -131,15 +139,14 @@ void optimizedSearch(set<int> x, vector<set<int>> F){
                     solMscPrint = solMscPrint + "S" + to_string(msc[i]+1) + " ";
                     ///imprimirSets(F[msc[i]]);
                 }
+                //Print menor a 60 para no generar un flood en la consola
                 if(msc.size()+mscSize < 60){
                     cout << solMscPrint << endl;
                 }
-                //cout << endl;
+                //Como encontro un msc, no tiene sentido seguir buscando...
                 break;
             }
         }
-        //
-
     }
 }
 
@@ -161,7 +168,7 @@ void greedAlgoritms(set<int> x, vector<set<int>> F){
                 if (elem != F.end()){
                     F.erase(elem);
                 }
-            
+
             }
         }
         mayor= 0;
@@ -180,7 +187,7 @@ void greedAlgoritms(set<int> x, vector<set<int>> F){
 void OptimizedGreedAlgoritms(set<int> x, vector<set<int>> F){
     cout << "Comenzando solucion 4......" << endl;
 
-    //Iterador 
+    //Iterador
     int k= 0;
     set<int>::iterator ite;
     //Algortimo presentado en clases
@@ -189,7 +196,7 @@ void OptimizedGreedAlgoritms(set<int> x, vector<set<int>> F){
     set<int> S;
     int count = 0;
     while( U.size() > 0 && k < 1){
-        
+
         for(size_t j = 0; j < F.size(); j++){
             for (ite = F.at(j).begin(); ite != F.at(j).end(); ite++) {
                 cout << *ite << " ";
@@ -212,8 +219,6 @@ void OptimizedGreedAlgoritms(set<int> x, vector<set<int>> F){
 }
 
 int main(){
-
-
     /*
     set<int> A = {1,2,3};
     set<int> B = {2,3,4,5};
@@ -233,14 +238,12 @@ int main(){
     // Set de ejemplos
     */
     set<int> S1 = {1,2,3,4,5,6};
-    //for(auto i=S1.begin();i!=S1.end();i++) cout << &i << endl;
     set<int> S2= {5,6,8,9};
     set<int> S3= {1,4,7,10};
     set<int> S4 = {2,3,5,7,8,11};
     set<int> S5 = {3,6,9,12};
     set<int> S6 = {10,11};
     set<int> A1 = {13,14,15,16,17,18};
-    //for(auto i=S1.begin();i!=S1.end();i++) cout << &i << endl;
     set<int> A2= {13,14,15,16,17,18,19,20};
     set<int> A3= {21,22};
     set<int> A4 = {22,23,24};
@@ -258,96 +261,55 @@ int main(){
     F.push_back(A3);
     F.push_back(A4);
 
-    int val = 1;
-    int val_2 = 2;
-    int val_3 = 3;
-    int* ptr_val = &val;
-    set<int*> T;
-    T.insert(ptr_val);
-    ptr_val = &val_2;
-    T.insert(ptr_val);
-    ptr_val = &val_3;
-    T.insert(ptr_val);
-    /*
-    for(auto i=T.begin();i!=T.end();i++) cout << "Direccion: " << *i << " / Valor: " << **i << endl;
-    cout << "---------------------\n";
-    set<int*> R;
-    R.insert(&val_2);
-    R.insert(&val_3);
-    for(auto i=R.begin();i!=R.end();i++) cout << "Direccion: " << *i << " / Valor: " << **i << endl;
-    */
-    //F[1].erase(F[1].find(3));
-    /*set<int> intersect;
-    set_intersection(S1.begin(),S1.end(),S2.begin(),S2.end(), inserter(intersect, intersect.begin()));
-    set<int> unionLessIntersect;
-    set<int> test = {1,2,3,4,8,9};
-    set_symmetric_difference(test.begin(),test.end(),S3.begin(),S3.end(), inserter(unionLessIntersect, unionLessIntersect.begin()));
-    */
-    
     //auto pos = S6.find(10);
     //cout << *pos << endl;
-    //exhaustiveSearch(x,F);
+
 
     set<int> x;
     x = getUniverse(F);
     //imprimirSets(x);
     auto start = std::chrono::high_resolution_clock::now();
-    //optimizedSearch(x,F);
-    
-    OptimizedGreedAlgoritms(x,F);
+    //exhaustiveSearch(x,F);
+    optimizedSearch(x,F);
+    //greedAlgoritms(x,F);
+    //OptimizedGreedAlgoritms(x,F);
     //std::this_thread::sleep_for(std::chrono::seconds (3));
     auto end = std::chrono::high_resolution_clock::now();
     auto int_s = std::chrono::duration_cast<std::chrono::seconds>(end - start);
     std::chrono::duration<double, std::milli> float_ms = end - start;
     std::cout << "funcSleep() elapsed time is " << float_ms.count() << " milliseconds" << std::endl;
     setsLectura();
-    /*it = s1.find(4);
-    bool isIn = s1.find(3) != s1.end();
-    cout << isIn << endl;
-    it = s1.end();
-    cout << *it;
-    */
-    //Iteracion sobre un set
-    /*
-    for (auto i = unionLessIntersect.begin(); i != unionLessIntersect.end(); i++)
-    {
-        cout << *i << " ";
-    }
-    */
 }
 
 void setsLectura(){
- string line ="";
- string sub;
-  ifstream myfile ("lectura.txt");
-  string space_delimiter = " ";
-  vector <set<int>> F;
-  set <int> A;
-  size_t pos = 0;
-  int c = 1;
-  if (myfile.is_open())
-  {
-    while ( getline (myfile,line) && c<2 )
-    {
-      //cout << line << '\n';
-        cout<<line << "\n";
-        while ((pos = line.find(space_delimiter)) != string::npos ) {
-        sub = line.substr(0, pos);
-        cout << stoi(sub) ;
-        //cout << stoi(sub);
-        //A.insert(sub);
-        //F.push_back(A);
-        line.erase(0, pos + space_delimiter.length());
-    }
+    string line ="";
+    string sub;
+    ifstream myfile ("lectura.txt");
+    string space_delimiter = " ";
+    vector <set<int>> F;
+    set <int> A;
     size_t pos = 0;
-    
+    int c = 1;
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) && c<2 )
+        {
+        //cout << line << '\n';
+            cout<<line << "\n";
+            while ((pos = line.find(space_delimiter)) != string::npos ) {
+            sub = line.substr(0, pos);
+            cout << stoi(sub) ;
+            //cout << stoi(sub);
+            //A.insert(sub);
+            //F.push_back(A);
+            line.erase(0, pos + space_delimiter.length());
+        }
+        size_t pos = 0;
+        }
+        myfile.close();
+        cout<<"en"<<endl;
+        imprimirVector(F);
     }
-    myfile.close();
-    cout<<"en"<<endl;
-    imprimirVector(F);
-   
-  }
-
-  else cout << "Unable to open file"; 
+    else cout << "Unable to open file";
 
 }
